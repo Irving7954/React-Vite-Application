feca4a21ae25d3c461296812f165b4132161e889
Luke Schoeberle <lukeschoeberle@gmail.com>
2026-02-03 12:39:38 -0600

    Updated the files through the React Render HTML section, which shows the code in the table instead of the code in App.jsx for the moment. This will probably be changed later.

--- src/App.jsx
+++ src/App.jsx
@@ -28,10 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-	/*<div className="App">
-      <h1>Hello World!</h1>
-    </div>*/	
+    </>	
   )
 }
 
