9aead6ea26f6a09fce91d283a48082f636c350bb
Luke Schoeberle <lukeschoeberle@gmail.com>
2026-02-06 15:16:32 -0600

    Added some minor changes relevant through the React Props Children section, which are more interesting since they involve more complex ways of passing props and style changes.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App({ name, age }) {
+function App(props) {
+	const { brand = "Hummer", name = "Bob", age = 90 } = props;
 	const [count, setCount] = useState(0);
 	
 	return (
-		<div>
-			<h1>Hello, {name}! You are {age} years old.</h1>
+		<div style={{background: 'lightblue'}}>
+			<h4>Hi, I am a {brand}!</h4>
+			<div>{props.children} </div>
+			<h3>Hello, {name}! You are {age} years old.</h3>
 			<button onClick={() => setCount(count + 1)}>
 				Count: {count}
 			</button>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const fruitlist = ['apple', 'banana', 'cherry'];
-
 createRoot(document.getElementById('root')).render(
-	<App name={"Tim"} age={30} />
+	<>
+		<h2>This should be white since the parent component has no style!</h2>
+		<App /*brand={"Ferrari"} 
+			name={"Tim"} 
+			--age={30}*/>
+			<h2>This should be blue since the child component has a blue style!</h2>
+		</App>
+	</>
 )
