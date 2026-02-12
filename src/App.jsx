defe1c6a1e007c583a96108c31f205eb3aa04899
Luke Schoeberle <lukeschoeberle@gmail.com>
2026-02-12 13:56:38 -0600

    Added some minor changes relevant through the React Checkbox section, which are actually interesting since they involve realistic and slightly more advanced actions, such as controlling the enabled forms' status.

--- src/App.jsx
+++ src/App.jsx
@@ -4,51 +4,68 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App(props) {
-	const { brandList = ["Hummer"], useBrands = false} = props;
+	const { brandList = ["Hummer"], useBrands = false } = props;
 	
-	const [count, setCount] = useState(0);
-	const [inputs, setInputs] = useState({brand: brandList[0]});
+	const [inputs, setInputs] = useState({brand: brandList[0], isEnabled: true});
 	
 	const handleChange = (e) => {
-		const name = e.target.name;
-		const value = e.target.value;
-		setInputs(values => ({...values, [name]: value}))
+		console.log(inputs.isEnabled);
+		if(inputs.isEnabled) {
+			const target = e.target;
+			const name = target.name;
+			const value = target.type == 'checkbox' ? target.checked : target.value;
+			setInputs(values => ({...values, [name]: value}))
+		}
 	}
 	
-	function handleSubmit(e) {
+	function handleNameSubmit(e) {
 		e.preventDefault();
 		alert(inputs.firstName);
+	}
+	
+	function handleBrandSubmit(e) {
+		e.preventDefault();
+		alert(inputs.brand);
+	}
+	
+	function handleTxtSubmit(e) {
+		e.preventDefault();
+		alert(inputs.txt);
+	}
+	
+	function handleIsEnabledSubmit(e) {
+		e.preventDefault();
+		alert(inputs.isEnabled);
 	}
 	
 	return (
 		<div style={{background: 'lightgreen'}}>
 			{useBrands && (
-			<form>
-				<select name="brand" value={inputs.brand} onChange={handleChange}>
-					{brandList.map((brand, index) => <option key={index} value={brand}>{brand}</option>)}
-				</select>
-				<p>Current brand: {inputs.brand}</p>
-			</form>	
+				<>
+					<form onSubmit={handleBrandSubmit}>
+						<label>Enter your brand:
+							<select name="brand" value={inputs.brand} onChange={handleChange}>
+							{brandList.map((brand, index) => <option key={index} value={brand}>{brand}</option>)}
+							</select>
+						</label>
+						<input type="submit" />
+					</form>
+				</>
 		    )}
 			
-			<div>{props.children} </div>
-			<button onClick={() => setCount(count + 1)}>
-				Count: {count}
-			</button>
-			<form onSubmit={handleSubmit}>
-				<label>Enter your name:
+			<form onSubmit={handleNameSubmit} disabled={!inputs.isEnabled}>
+				<label>Enter your name:			
 					<input
 						type="text"
 						name="firstName"
-						value={inputs.firstname}
+						value={inputs.firstName}
 						onChange={handleChange}
 					/>
 				</label>
-				<p>Current name: {inputs.firstName}</p>
 				<input type="submit" />
 			</form>
 			
-			<form>
+			<form onSubmit={handleTxtSubmit} disabled={!inputs.isEnabled}>
 				<label>Write here:
 					<textarea
 						name="txt"
@@ -56,8 +73,22 @@ function App(props) {
 						onChange={handleChange}
 					/>
 				</label>
-				<p>Current text: {inputs.txt}</p>
-			</form>	
+				<input type="submit" />
+			</form>
+			
+			<form onSubmit={handleIsEnabledSubmit}>
+				<label>Enable the forms:			
+					<input
+						type="checkbox"
+						name="isEnabled"
+						checked={inputs.isEnabled}
+						onChange={handleChange}
+					/>
+				</label>
+				<input type="submit" />
+			</form>
+			
+			<div>{props.children} </div>
 		</div>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 	<>
-		<h2>The main component has no style!</h2>
+		<h2>The main component is white!</h2>
 		<App brandList={["Ferrari", "Porsche", "McClaren"]}
 			useBrands={true}>
-			<h2>This header should be light green without the style!</h2>
+			<h2>This inner header is light green automatically!</h2>
 		</App>
 	</>
 )
